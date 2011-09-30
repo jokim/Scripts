ac219b2b8c3c1a6bcafbc360b07db9c43e1ef1c4
Joakim S. Hovlandsvåg <joakim.hovlandsvag@gmail.com>
2011-09-30 21:24:03 +0200

    Added the statistic of use of normal characters.

--- cryptography/analyze_cipher.py
+++ cryptography/analyze_cipher.py
@@ -7,8 +7,40 @@ A simple script for doing a basic analysis of a given ciphertext. It counts up
 the number of occurrences of each character, which is usable if the encryption
 is done with plain substitutions.
 """
-
 import sys
+
+# Statistical use of Norwegian letters
+char_use_no = {
+        'a': 6.1,
+        'b': 1.5,
+        'c': 0.2,
+        'd': 4.3,
+        'e': 15.2,
+        'f': 2.0,
+        'g': 3.8,
+        'h': 1.6,
+        'i': 6.2,
+        'j': 1.0,
+        'k': 3.8,
+        'l': 5.4,
+        'm': 3.3,
+        'n': 8.1,
+        'o': 4.9,
+        'p': 1.9,
+        'q': 0.004,
+        'r': 8.6,
+        's': 6.7,
+        't': 7.9,
+        'u': 1.6,
+        'v': 2.5,
+        'w': 0.1,
+        'x': 0.03,
+        'y': 0.7,
+        'z': 0.03,
+        'æ': 0.2,
+        'ø': 0.9,
+        'å': 1.5,
+        }
 
 def count_chars(input):
     """Count the number of occurrences of each character and return a dict."""
